02ff91fbd38b74642429cc2bd809ae1c8565678b
Dario Santacruz <git@dariosantacruz.com>
2011-07-19 18:39:39 -0700

    Se agregó el archivo de la página de servicios en blanco

--- elementos-de-la-pagina/navegacion.php
+++ elementos-de-la-pagina/navegacion.php
@@ -1,5 +1,5 @@
 <li><a href="/index.php" target="_self">Inicio</a></li>
-<li><a href="#" target="_self">Servicios</a></li>
+<li><a href="/servicios.php" target="_self">Servicios</a></li>
 <li><a href="/curriculum.php" target="_self">Currículum</a></li>
-<li><a href="testimonios.php" target="_self">Testimonios</a></li>
-<li><a href="contacto.php" target="_self">Contacto</a></li>
+<li><a href="/testimonios.php" target="_self">Testimonios</a></li>
+<li><a href="/contacto.php" target="_self">Contacto</a></li>

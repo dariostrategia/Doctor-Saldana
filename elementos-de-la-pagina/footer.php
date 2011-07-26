c7caeaa7eac7c5f77d5678f38a6795463ff536cc
Dario Santacruz <git@dariosantacruz.com>
2011-07-25 18:54:31 -0700

    Corrección al pie de página para que funcione con el Slider. Se anexan cambios al robots.txt

--- elementos-de-la-pagina/footer.php
+++ elementos-de-la-pagina/footer.php
@@ -1,10 +1,10 @@
 <div class="servicios_index">
 	<h1>Servicios</h1>
 	<div class="cuadro_servicios">
-		<a href="http://google.com" class="servicio rojo_s     servicio_lnk" target="_new">Gingivitis</a>
-		<a href="http://google.com" class="servicio aqua_s     servicio_lnk" target="_new">Periodontitis</a>
-		<a href="http://google.com" class="servicio verde_s    servicio_lnk" target="_new">Implantes</a>
-		<a href="http://google.com" class="servicio amarillo_s servicio_lnk" target="_new">Recontorneo</a>
-		<a href="http://google.com" class="servicio naranja_s  servicio_lnk" target="_new">Recesión</a>
+		<a href="/servicios#1" class="servicio rojo_s     servicio_lnk" target="_new">Gingivitis</a>
+		<a href="/servicios#2" class="servicio aqua_s     servicio_lnk" target="_new">Periodontitis</a>
+		<a href="/servicios#3" class="servicio verde_s    servicio_lnk" target="_new">Implantes</a>
+		<a href="/servicios#4" class="servicio amarillo_s servicio_lnk" target="_new">Recontorneo</a>
+		<a href="/servicios#5" class="servicio naranja_s  servicio_lnk" target="_new">Recesión</a>
 	</div>
 </div>

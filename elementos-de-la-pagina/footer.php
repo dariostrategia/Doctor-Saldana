62e72849f98a128690c4c2b80791c55cd1a5b715
Dario Santacruz <git@dariosantacruz.com>
2011-07-26 12:32:04 -0700

    Corregí unos errores en los enlaces del pié de página, ajusté los colores a los exactos del PSD, aunque estaba usando los colores que ya me habían dado en el CSS original de la página, así que que retuvieran desajustados no es mi culpa, ¿ven por qué es bueno tener poquitos? y coloreé los cuadros de los servicios. Me faltan las fotos, esas las agregaré con image sprites.

--- elementos-de-la-pagina/footer.php
+++ elementos-de-la-pagina/footer.php
@@ -1,10 +1,10 @@
 <div class="servicios_index">
 	<h1>Servicios</h1>
 	<div class="cuadro_servicios">
-		<a href="/servicios#1" class="servicio rojo_s     servicio_lnk" target="_new">Gingivitis</a>
-		<a href="/servicios#2" class="servicio aqua_s     servicio_lnk" target="_new">Periodontitis</a>
-		<a href="/servicios#3" class="servicio verde_s    servicio_lnk" target="_new">Implantes</a>
-		<a href="/servicios#4" class="servicio amarillo_s servicio_lnk" target="_new">Recontorneo</a>
-		<a href="/servicios#5" class="servicio naranja_s  servicio_lnk" target="_new">Recesión</a>
+		<a href="/servicios.php#1" class="servicio rojo_s     servicio_lnk" target="_new">Gingivitis</a>
+		<a href="/servicios.php#2" class="servicio aqua_s     servicio_lnk" target="_new">Periodontitis</a>
+		<a href="/servicios.php#3" class="servicio verde_s    servicio_lnk" target="_new">Implantes</a>
+		<a href="/servicios.php#4" class="servicio amarillo_s servicio_lnk" target="_new">Recontorneo</a>
+		<a href="/servicios.php#5" class="servicio naranja_s  servicio_lnk" target="_new">Recesión</a>
 	</div>
 </div>
